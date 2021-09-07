60cc2d764f8dc9e02d5d986689b0f6ee2d61a616
OldMan35 <kostya_kv@mail.ru>
2021-09-07 23:52:19 +0300

    fix bags

--- src/main/java/org/example/AppMortgageApplication.java
+++ src/main/java/org/example/AppMortgageApplication.java
@@ -1,9 +1,15 @@
 package org.example;
 
 
+import org.example.model.MortgageClients;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AppMortgageApplication {
     public static void main(String[] args) {
-
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MortgageClients mortgageClients = context.getBean("mortgageClients", MortgageClients.class);
+        System.out.println(mortgageClients.getName());
+        context.close();
     }
 
 }
--- src/main/java/org/example/model/MortgageClients.java
+++ src/main/java/org/example/model/MortgageClients.java
@@ -1,5 +1,17 @@
 package org.example.model;
 
 public class MortgageClients {
+    private String name;
 
+    public MortgageClients(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
