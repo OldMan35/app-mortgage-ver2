a64b2215576a9b4598e8fa4c0141a0dd225bc983
OldMan35 <kostya_kv@mail.ru>
2021-09-10 02:37:11 +0300

    fix bag

--- src/main/java/org/example/service/MortgageService.java
+++ src/main/java/org/example/service/MortgageService.java
@@ -1,7 +1,6 @@
 package org.example.service;
 
 import org.example.repository.MortgageRepository;
-import org.springframework.context.annotation.Bean;
 
 
 public class MortgageService implements MortgageRepository {
