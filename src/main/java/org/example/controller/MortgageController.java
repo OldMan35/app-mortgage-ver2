f59e1ec3feca92bcfc522adabe8450f7278100a3
OldMan35 <kostya_kv@mail.ru>
2021-09-09 23:26:21 +0300

    add MortgageController.java, MortgageClients.java

--- src/main/java/org/example/controller/MortgageController.java
+++ src/main/java/org/example/controller/MortgageController.java
@@ -1,8 +1,35 @@
 package org.example.controller;
 
+import org.example.model.MortgageClients;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 @Controller
+@RequestMapping(value = "/mortgage")
 public class MortgageController {
 
+
+    @GetMapping(value = "/get-all")
+    public ResponseEntity<?> getAll() {
+    //    List<MortgageClients> mortgageClientsList = mortgageClientsService.getAll();
+    //    if (mortgageClientsList.size() != 0) {
+     //       return ResponseEntity.ok().body(mortgageClientsList);
+     //   } else {
+            return new ResponseEntity<String>("Заявок нет", HttpStatus.OK);
+     //   }
+
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<?> createMortgageClients(@RequestBody MortgageClients mortgageClients) {
+       // mortgageClientsService.create(mortgageClients);
+        return new ResponseEntity<String>("Заявка создана", HttpStatus.CREATED);
+    }
 }
+
--- src/main/java/org/example/model/MortgageClients.java
+++ src/main/java/org/example/model/MortgageClients.java
@@ -1,17 +1,45 @@
 package org.example.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MortgageClients {
-    private String name;
 
-    public MortgageClients(String name) {
-        this.name = name;
-    }
+    private int id;
 
-    public String getName() {
-        return name;
-    }
+    private String nameOfBuyers;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private String patronymicOfBuyers;
+
+    private String surnameOfBuyers;
+
+    private String phoneNumberOfBuyers;
+
+    private long loanAmount;
+
+    private int loanTerm;
+
+    private String estateObject;
+
+    private long costObject;
+
+    private String innOfBuyers;
+
+    private String nameOfSellers;
+
+    private String patronymicOfSellers;
+
+    private String surnameOfSellers;
+
+    private String innInd;
+
+    private String nameOrganization;
+
+    private String innOrg;
+
+    private String ownRights;
 }
+
