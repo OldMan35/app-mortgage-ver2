785c85a813d749028e9903605da9330906da8974
OldMan35 <kostya_kv@mail.ru>
2021-09-10 02:30:03 +0300

    add MortgageRepository, MortgageService

---
+++ src/main/java/org/example/service/MortgageService.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+import org.example.repository.MortgageRepository;
+import org.springframework.context.annotation.Bean;
+
+
+public class MortgageService implements MortgageRepository {
+
+}
