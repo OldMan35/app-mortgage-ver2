ff424485160dcc8de4ca23f6f227ec4d7a11bb95
OldMan35 <kostya_kv@mail.ru>
2021-09-10 02:18:33 +0300

    fix bag

--- src/main/java/org/example/controller/MortgageController.java
+++ src/main/java/org/example/controller/MortgageController.java
@@ -8,28 +8,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
 @Controller
 @RequestMapping(value = "/mortgage")
 public class MortgageController {
 
+    @GetMapping(value = "/hello")
+    public ResponseEntity<String> hello (){
+        return new ResponseEntity<String>("hello",HttpStatus.OK);
+    }
 
-    @GetMapping(value = "/get-all")
-    public ResponseEntity<?> getAll() {
+
+//    @GetMapping(value = "/get-all")
+//    public ResponseEntity<?> getAll() {
     //    List<MortgageClients> mortgageClientsList = mortgageClientsService.getAll();
     //    if (mortgageClientsList.size() != 0) {
-     //       return ResponseEntity.ok().body(mortgageClientsList);
-     //   } else {
-            return new ResponseEntity<String>("Заявок нет", HttpStatus.OK);
-     //   }
+    //       return ResponseEntity.ok().body(mortgageClientsList);
+    //   } else {
+//            return new ResponseEntity<String>("Заявок нет", HttpStatus.OK);
+    //   }
 
-    }
+//    }
 
-    @PostMapping(value = "/create")
-    public ResponseEntity<?> createMortgageClients(@RequestBody MortgageClients mortgageClients) {
-       // mortgageClientsService.create(mortgageClients);
-        return new ResponseEntity<String>("Заявка создана", HttpStatus.CREATED);
-    }
+//    @PostMapping(value = "/create")
+//    public ResponseEntity<?> createMortgageClients(@RequestBody MortgageClients mortgageClients) {
+    // mortgageClientsService.create(mortgageClients);
+//        return new ResponseEntity<String>("Заявка создана", HttpStatus.CREATED);
+//    }
 }
 
