015011e02dc958c41b235e67b0539c4a45d61449
OldMan35 <kostya_kv@mail.ru>
2021-09-11 06:57:27 +0300

    fix bag222

--- src/main/java/org/example/service/MortgageService.java
+++ src/main/java/org/example/service/MortgageService.java
@@ -10,20 +10,20 @@ import java.util.List;
 @Service
 public class MortgageService {
 
-    private final MortgageRepository mortgageRepository;
-
-//    @Autowired
-    public MortgageService(MortgageRepository mortgageRepository) {
-        this.mortgageRepository = mortgageRepository;
-    }
-
-    public List<MortgageClients> getAll() {
-        return mortgageRepository.findAll();
-    }
-
-    public void create(MortgageClients mortgageClients) {
-        mortgageRepository.save(mortgageClients);
-    }
+//    private final MortgageRepository mortgageRepository;
+//
+////    @Autowired
+//    public MortgageService(MortgageRepository mortgageRepository) {
+//        this.mortgageRepository = mortgageRepository;
+//    }
+//
+//    public List<MortgageClients> getAll() {
+//        return mortgageRepository.findAll();
+//    }
+//
+//    public void create(MortgageClients mortgageClients) {
+//        mortgageRepository.save(mortgageClients);
+//    }
 
 
 }
