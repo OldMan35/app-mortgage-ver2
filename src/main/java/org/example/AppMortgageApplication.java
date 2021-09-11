8e5db2639c605b835a129967ca56135ed7d92f0d
OldMan35 <kostya_kv@mail.ru>
2021-09-11 21:09:56 +0300

    fix bag

--- src/main/java/org/example/AppMortgageApplication.java
+++ src/main/java/org/example/AppMortgageApplication.java
@@ -1,11 +1,9 @@
 package org.example;
 
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppMortgageApplication {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
     }
 
--- src/main/java/org/example/controller/MortgageController.java
+++ src/main/java/org/example/controller/MortgageController.java
@@ -4,6 +4,7 @@ import org.example.model.MortgageClients;
 import org.example.service.MortgageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -36,7 +37,7 @@ public class MortgageController {
        }
     }
 
-    @PostMapping(value = "/create")
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createMortgageClients(@RequestBody MortgageClients mortgageClients) {
      mortgageService.create(mortgageClients);
         return new ResponseEntity<String>("Заявка создана", HttpStatus.CREATED);
