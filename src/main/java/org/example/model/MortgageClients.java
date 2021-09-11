d787cd40db0f490a279420eb818642f17f047604
OldMan35 <kostya_kv@mail.ru>
2021-09-11 20:12:25 +0300

    add annotation

--- src/main/java/org/example/model/MortgageClients.java
+++ src/main/java/org/example/model/MortgageClients.java
@@ -1,5 +1,6 @@
 package org.example.model;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
@@ -16,36 +17,71 @@ public class MortgageClients {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotNull
+    @Column(name = "nameofbuyers")
     private String nameOfBuyers;
 
+    @NotNull
+    @Column(name = "patronymicofbuyers")
     private String patronymicOfBuyers;
 
+    @NotNull
+    @Column(name = "surnameofbuyers")
     private String surnameOfBuyers;
 
+    @NotNull
+    @Column(name = "phonenumberofbuyers")
     private String phoneNumberOfBuyers;
 
+    @NotNull
+    @Column(name = "loanamount")
     private long loanAmount;
 
+    @NotNull
+    @Column(name = "loanterm")
     private int loanTerm;
 
+    @NotNull
+    @Column(name = "estateobject")
     private String estateObject;
 
+    @NotNull
+    @Column(name = "costobject")
     private long costObject;
 
+    @NotNull
+//    @InnValidation
+    @Column(name = "innofbuyers")
     private String innOfBuyers;
 
+    @NotNull
+    @Column(name = "nameofsellers")
     private String nameOfSellers;
 
+    @NotNull
+    @Column(name = "patronymicofsellers")
     private String patronymicOfSellers;
 
+    @NotNull
+    @Column(name = "surnameofsellers")
     private String surnameOfSellers;
 
+    @NotNull
+    @Column(name = "innind")
+//    @InnValidation
     private String innInd;
 
+    @NotNull
+    @Column(name = "nameorganization")
     private String nameOrganization;
 
+    @NotNull
+    @Column(name = "innorg")
+//    @InnValidation
     private String innOrg;
 
+    @NotNull
+    @Column(name = "ownrights")
     private String ownRights;
 }
 
