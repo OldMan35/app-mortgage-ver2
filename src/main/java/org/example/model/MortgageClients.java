cae2d3fdb54312f30a8773370f063115e0e328bb
OldMan35 <kostya_kv@mail.ru>
2021-09-12 11:07:50 +0300

    fax bag

--- src/main/java/org/example/model/MortgageClients.java
+++ src/main/java/org/example/model/MortgageClients.java
@@ -13,7 +13,6 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-//@Table(name = "mortgage")
 public class MortgageClients {
 
     @Id
@@ -21,67 +20,51 @@ public class MortgageClients {
     private int id;
 
     @NotNull
-//    @Column(name = "nameofbuyers")
     private String nameOfBuyers;
 
     @NotNull
-//    @Column(name = "patronymicofbuyers")
     private String patronymicOfBuyers;
 
     @NotNull
-    //  @Column(name = "surnameofbuyers")
     private String surnameOfBuyers;
 
     @NotNull
-    //  @Column(name = "phonenumberofbuyers")
     private String phoneNumberOfBuyers;
 
     @NotNull
-    //  @Column(name = "loanamount")
     private long loanAmount;
 
     @NotNull
-    //   @Column(name = "loanterm")
     private int loanTerm;
 
     @NotNull
-    //  @Column(name = "estateobject")
     private String estateObject;
 
     @NotNull
-    //   @Column(name = "costobject")
     private long costObject;
 
     @NotNull
-    //  @Column(name = "innofbuyers")
     private String innOfBuyers;
 
     @NotNull
-    //  @Column(name = "nameofsellers")
     private String nameOfSellers;
 
     @NotNull
-    //  @Column(name = "patronymicofsellers")
     private String patronymicOfSellers;
 
     @NotNull
-    //   @Column(name = "surnameofsellers")
     private String surnameOfSellers;
 
     @NotNull
-    //   @Column(name = "innind")
     private String innInd;
 
     @NotNull
-    //   @Column(name = "nameorganization")
     private String nameOrganization;
 
     @NotNull
-    //  @Column(name = "innorg")
     private String innOrg;
 
     @NotNull
-    //  @Column(name = "ownrights")
     private String ownRights;
 }
 
